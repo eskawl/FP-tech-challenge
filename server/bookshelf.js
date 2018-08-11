8517f64cf2333c3301322241c1e71080d13e8660
srutesh <sruteshp@gmail.com>
2018-08-11 11:18:58 +0530

    Prepare for heroku

--- server/bookshelf.js
+++ server/bookshelf.js
@@ -1,11 +1,21 @@
-const knex = require('knex')({
-    client: 'mysql',
-    connection: {
+const devMode = process.env.NODE_ENV === 'development';
+
+let connection;
+
+if (devMode) {
+    connection= {
         host: '127.0.0.1',
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB,
     }
+} else {
+    connection = process.env.CLEARDB_DATABASE_URL
+}
+
+const knex = require('knex')({
+    client: 'mysql',
+    connection,
 });
 
 const bookshelf = require('bookshelf')(knex);
